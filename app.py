366297ae63d0502de97a862a7d02caf287c1da3a
dewasemadi <dewasemadi@apps.ipb.ac.id>
2021-03-20 00:10:41 +0800

    make app.py easy to use

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
 
-data = pd.read_excel(r'D:\Development\sertificate-generator-with-py\list.xlsx')
+# change directory with your excel and image file directory
+xlsx_dir = r"D:\Development\sertificate-generator-with-py\list.xlsx"
+sertificate_dir = r"D:\Development\sertificate-generator-with-py\sertificate.jpg"
+
+data = pd.read_excel(xlsx_dir)
 name_list = data["Name"].tolist()
 
 for i in name_list:
-    img = Image.open(
-        r'D:\Development\sertificate-generator-with-py\sertificate.jpg')
+    img = Image.open(sertificate_dir)
     draw = ImageDraw.Draw(img)
 
     font_size = 200
@@ -16,9 +19,9 @@ for i in name_list:
     center_x_position = (img.width-text_width)/2
     text_y_position = 800
 
-    location = (center_x_position, text_y_position)
+    location_x_y = (center_x_position, text_y_position)
     # RGB color
     text_color = (0, 0, 0)
 
-    draw.text(location, i, fill=text_color, font=font)
+    draw.text(location_x_y, i, fill=text_color, font=font)
     img.save("certificate_" + i + ".pdf")
